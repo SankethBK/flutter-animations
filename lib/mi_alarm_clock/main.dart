add8ed03ffe7b8fa990b05bf5294c8e9b075fdf0
Sanketh B K <51091231+SankethBK@users.noreply.github.com>
2022-08-20 01:09:14 +0530

    modal route

--- lib/mi_alarm_clock/main.dart
+++ lib/mi_alarm_clock/main.dart
@@ -10,57 +10,84 @@ class MIAlarmCustomModalAnatiomPage extends StatelessWidget {
       appBar: AppBar(title: const Text("MI Alarm Clock")),
       body: ListView(
         children: [
-          AlarmItem("06:33"),
-          AlarmItem("04:53"),
-          AlarmItem("07:23"),
-          AlarmItem("08:34"),
-          AlarmItem("04:31"),
-          AlarmItem("01:23"),
-          AlarmItem("06:32"),
-          AlarmItem("06:33"),
-          AlarmItem("08:23"),
-          AlarmItem("04:32"),
-          AlarmItem("03:23"),
-          AlarmItem("06:33"),
+          AlarmItem(context, "06:33"),
+          AlarmItem(context, "04:53"),
+          AlarmItem(context, "07:23"),
+          AlarmItem(context, "08:34"),
+          AlarmItem(context, "04:31"),
+          AlarmItem(context, "01:23"),
+          AlarmItem(context, "06:32"),
+          AlarmItem(context, "06:33"),
+          AlarmItem(context, "08:23"),
+          AlarmItem(context, "04:32"),
+          AlarmItem(context, "03:23"),
+          AlarmItem(context, "06:33"),
         ],
       ),
     );
   }
 
-  Widget AlarmItem(String alarmTime) {
-    return OpenContainer(
-      transitionDuration: const Duration(milliseconds: 3000),
-      openBuilder: (BuildContext context,
-          void Function({Object? returnValue}) closeContainer) {
-        return AlarmModal();
+  Widget AlarmItem(BuildContext context, String alarmTime) {
+    return ListTile(
+      onTap: () {
+        Navigator.of(context).push(AlarmModalRoute());
       },
-      closedBuilder: (BuildContext context, void Function() openContainer) {
-        return Hero(
-          tag: "e",
-          child: ListTile(
-            title: RichText(
-              text: TextSpan(
-                text: alarmTime,
-                style: const TextStyle(fontSize: 26, color: Colors.grey),
-                children: const [
-                  WidgetSpan(
-                      alignment: PlaceholderAlignment.baseline,
-                      baseline: TextBaseline.alphabetic,
-                      child: SizedBox(width: 5)),
-                  TextSpan(text: "pm", style: TextStyle(fontSize: 12))
-                ],
-              ),
-            ),
-            subtitle: const Text(
-              "Once",
-              style: TextStyle(color: Colors.grey),
-            ),
-            trailing: Switch(value: false, onChanged: (_) {}),
-          ),
-        );
-      },
+      title: RichText(
+        text: TextSpan(
+          text: alarmTime,
+          style: const TextStyle(fontSize: 26, color: Colors.grey),
+          children: const [
+            WidgetSpan(
+                alignment: PlaceholderAlignment.baseline,
+                baseline: TextBaseline.alphabetic,
+                child: SizedBox(width: 5)),
+            TextSpan(text: "pm", style: TextStyle(fontSize: 12))
+          ],
+        ),
+      ),
+      subtitle: const Text(
+        "Once",
+        style: TextStyle(color: Colors.grey),
+      ),
+      trailing: Switch(value: false, onChanged: (_) {}),
     );
   }
+}
+
+class AlarmModalRoute extends ModalRoute {
+  @override
+  Color? get barrierColor => Colors.red;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  String? get barrierLabel => "Alarm Modal";
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    throw FadeTransition(
+      opacity: animation,
+      child: Hero(
+        tag: "e",
+        child: Container(
+          color: Colors.pinkAccent,
+          height: 200,
+          width: 300,
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get maintainState => false;
+
+  @override
+  bool get opaque => false;
+
+  @override
+  Duration get transitionDuration => const Duration(seconds: 4);
 }
 
 class AlarmModal extends StatelessWidget {
