bfe15771b64edeb25d18911e35d6f9d3dbc1b748
Sanketh B K <51091231+SankethBK@users.noreply.github.com>
2022-08-21 12:03:02 +0530

    stack approach

--- lib/mi_alarm_clock/main.dart
+++ lib/mi_alarm_clock/main.dart
@@ -10,124 +10,89 @@ class MIAlarmCustomModalAnatiomPage extends StatelessWidget {
       appBar: AppBar(title: const Text("MI Alarm Clock")),
       body: ListView(
         children: [
-          AlarmItem(context, "06:33"),
-          AlarmItem(context, "04:53"),
-          AlarmItem(context, "07:23"),
-          AlarmItem(context, "08:34"),
-          AlarmItem(context, "04:31"),
-          AlarmItem(context, "01:23"),
-          AlarmItem(context, "06:32"),
-          AlarmItem(context, "06:33"),
-          AlarmItem(context, "08:23"),
-          AlarmItem(context, "04:32"),
-          AlarmItem(context, "03:23"),
-          AlarmItem(context, "06:33"),
+          AlarmItem(context: context, alarmTime: "06:33"),
+          AlarmItem(context: context, alarmTime: "04:53"),
+          AlarmItem(context: context, alarmTime: "07:23"),
+          AlarmItem(context: context, alarmTime: "08:34"),
+          AlarmItem(context: context, alarmTime: "04:31"),
+          AlarmItem(context: context, alarmTime: "01:23"),
+          AlarmItem(context: context, alarmTime: "06:32"),
+          AlarmItem(context: context, alarmTime: "06:33"),
+          AlarmItem(context: context, alarmTime: "08:23"),
+          AlarmItem(context: context, alarmTime: "04:32"),
+          AlarmItem(context: context, alarmTime: "03:23"),
+          AlarmItem(context: context, alarmTime: "06:33"),
         ],
       ),
     );
   }
-
-  Widget AlarmItem(BuildContext context, String alarmTime) {
-    return ListTile(
-      onTap: () {
-        Navigator.of(context).push(AlarmModalRoute());
-      },
-      title: RichText(
-        text: TextSpan(
-          text: alarmTime,
-          style: const TextStyle(fontSize: 26, color: Colors.grey),
-          children: const [
-            WidgetSpan(
-                alignment: PlaceholderAlignment.baseline,
-                baseline: TextBaseline.alphabetic,
-                child: SizedBox(width: 5)),
-            TextSpan(text: "pm", style: TextStyle(fontSize: 12))
-          ],
-        ),
-      ),
-      subtitle: const Text(
-        "Once",
-        style: TextStyle(color: Colors.grey),
-      ),
-      trailing: Switch(value: false, onChanged: (_) {}),
-    );
-  }
 }
 
-class AlarmModalRoute extends ModalRoute {
-  @override
-  Color? get barrierColor => Colors.red;
+class AlarmItem extends StatefulWidget {
+  const AlarmItem({
+    Key? key,
+    required this.context,
+    required this.alarmTime,
+  }) : super(key: key);
+
+  final BuildContext context;
+  final String alarmTime;
 
   @override
-  bool get barrierDismissible => true;
-
-  @override
-  String? get barrierLabel => "Alarm Modal";
-
-  @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
-    throw FadeTransition(
-      opacity: animation,
-      child: Hero(
-        tag: "e",
-        child: Container(
-          color: Colors.pinkAccent,
-          height: 200,
-          width: 300,
-        ),
-      ),
-    );
-  }
-
-  @override
-  bool get maintainState => false;
-
-  @override
-  bool get opaque => false;
-
-  @override
-  Duration get transitionDuration => const Duration(seconds: 4);
+  State<AlarmItem> createState() => _AlarmItemState();
 }
 
-class AlarmModal extends StatelessWidget {
-  const AlarmModal({Key? key}) : super(key: key);
+class _AlarmItemState extends State<AlarmItem> {
+  bool isOpen = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        height: 200,
-        width: 300,
-        color: Colors.blueAccent,
-        padding: const EdgeInsets.all(20.0),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Hero(
-            tag: "e",
-            child: ListTile(
-              title: RichText(
-                text: TextSpan(
-                  text: "06:00",
-                  style: const TextStyle(fontSize: 26, color: Colors.grey),
-                  children: const [
-                    WidgetSpan(
-                        alignment: PlaceholderAlignment.baseline,
-                        baseline: TextBaseline.alphabetic,
-                        child: SizedBox(width: 5)),
-                    TextSpan(text: "pm", style: TextStyle(fontSize: 12))
-                  ],
+    print("rebuilding listitem ${widget.alarmTime}");
+    return SizedBox(
+      height: 70,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          AnimatedPositioned(
+            // top: !isOpen ? 0 : -50,
+            bottom: !isOpen ? 0 : -50,
+            duration: const Duration(milliseconds: 1000),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 1000),
+              width: !isOpen
+                  ? MediaQuery.of(context).size.width
+                  : MediaQuery.of(context).size.width * 0.8,
+              color: !isOpen ? Colors.white : Colors.grey[100],
+              child: ListTile(
+                onTap: () {
+                  setState(() {
+                    isOpen = !isOpen;
+                  });
+                },
+                title: RichText(
+                  text: TextSpan(
+                    text: widget.alarmTime,
+                    style: const TextStyle(fontSize: 26, color: Colors.grey),
+                    children: const [
+                      WidgetSpan(
+                          alignment: PlaceholderAlignment.baseline,
+                          baseline: TextBaseline.alphabetic,
+                          child: SizedBox(width: 5)),
+                      TextSpan(text: "pm", style: TextStyle(fontSize: 12))
+                    ],
+                  ),
                 ),
+                subtitle: const Text(
+                  "Once",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                trailing: Switch(value: false, onChanged: (_) {}),
               ),
-              subtitle: const Text(
-                "Once",
-                style: TextStyle(color: Colors.grey),
-              ),
-              trailing: Switch(value: false, onChanged: (_) {}),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
